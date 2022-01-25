fb38dc62a0fb690fff2b7707587c9d57e5601572
amarpal <amarpalkr50@gmail.com>
2022-01-25 19:55:44 +0530

    server chnge

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:"http://localhost:5000/api/",
+  // apiUrl:"http://localhost:5000/api/",
+  apiUrl:"https://localhost:44392/api/",
 };
 
 /*
