bf9104fc4ea364ce17c780d8d772be6a15a5b64b
stdev <stdev.net@gmail.con>
2022-02-24 23:58:33 +0530

    chages

--- src/app/Components/pages/reports/correctionbydepart/correctionbydepart.component.ts
+++ src/app/Components/pages/reports/correctionbydepart/correctionbydepart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild,ElementRef  } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '@app/_services';
 import { reportOptionLoadingServices } from '@app/_services/reportOptionLoadingServices';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -9,7 +9,6 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { first } from 'rxjs';
 import html2canvas from 'html2canvas';
-
 
 @Component({
   selector: 'app-correctionbydepart',
@@ -22,10 +21,19 @@ export class CorrectionbydepartComponent implements OnInit {
   p: number = 1;
   options:any;
   printDate = new Date();
-  constructor(private formbuilder:FormBuilder, private modalService:NgbModal,private reportOptionLoadingServices:reportOptionLoadingServices, private spinner: NgxSpinnerService,private toastrService: ToastrService,private userService:UserService ) {
+  year: any;
+  storeName: any;
+  form: FormGroup;
+  modalClass: string | undefined;
+  departments: any;
+
+  constructor(private formbuilder:FormBuilder, private modalService:NgbModal,private reportOptionLoadingServices:reportOptionLoadingServices, private spinner: NgxSpinnerService,private toastrService: ToastrService,private userService:UserService,private fb: FormBuilder) {
     this.getLabelInformation();
-
-
+    this.form = this.fb.group({
+      checkArray: this.fb.array([])
+    })
+    this.userService.stockDate.subscribe(user => this.year = user);
+    this.userService.storeName.subscribe(user => this.storeName = user);
    }
 
   ngOnInit(): void 
@@ -50,16 +58,38 @@ this.options = {
     .pipe(first())
     .subscribe({
       next: (response: any) => {
-        this.spinner.hide();
+        debugger
        this.reportList=response;
+       this.departments = [...new Set(response.map((x: { department: any; }) => x.department))];       
+       this.spinner.hide();
       }
     });
   }
 
+  filterDepartment(){
+   
+     this.reportOptionLoadingServices.getCorrectionsReportInformation()
+    .pipe(first())
+    .subscribe({
+      next: (response: any) => {
+       this.reportList=response;
+       this.departments = [...new Set(response.map((x: { department: any; }) => x.department))];       
+       this.spinner.hide();
+       let s = this.form.value.checkArray;
+       var result = this.reportList.filter(function(e:any) {
+        return [s].includes(e.department.toString())
+      });
+      this.reportList = result;
+      this.modalService.dismissAll();
+      }
+      
+    });
+
+  }
   openPDF() {
+
     const response = document.getElementById('htmlData') as HTMLElement;        
     html2canvas(response).then(canvas => { 
-
         let fileWidth = 208;
         let fileHeight = canvas.height * fileWidth / canvas.width;
         
@@ -70,5 +100,29 @@ this.options = {
         
         PDF.save('recordOptions.pdf');
     });     
+    }
+    open(content: any) {
+      this.modalClass = 'mymodal',
+      this.modalService.open(content, {
+        ariaLabelledBy: 'modal-basic-title',
+        windowClass: 'modal-filteradd'
+      }).result.then((result) => {
+      }, (reason) => {
+      });
+    }
+    onCheckboxChange(e:any) {
+      const checkArray: any = this.form.get('checkArray') as FormArray;
+      if (e.target.checked) {
+        checkArray.push(new FormControl(e.target.value));
+      } else {
+        let i: number = 0;
+        checkArray.controls.forEach((item: any) => {
+          if (item.value == e.target.value) {
+            checkArray.removeAt(i);
+            return;
+          }
+          i++;
+        });
+      }
     }
 }
